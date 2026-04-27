9e00f6035ca725a80095b09b99fe31369e04912f
Nagasi77 <2341720260@student.polinema.ac.id>
2026-04-27 21:50:57 +0700

    update

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,12 +1,9 @@
-import { useSession } from "next-auth/react"
-
-const HalamanProfile = () => {
-    const { data }: any = useSession()
-    return (
-        <div>
-            <h1>Halaman Profile</h1>
-            <br />
-            <h1>Selamat Datang {data?.user?.fullname}</h1>
-        </div>
-    )
+const ProfilePage = () => {
+  return (
+    <div>
+      <h1>Halaman Profil</h1>
+    </div>
+  )
 }
+
+export default ProfilePage
